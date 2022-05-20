68b77c25517d0be9b3398d06ea56780cf2659147
EC2 Default User <ec2-user@ip-172-31-35-116.ap-northeast-1.compute.internal>
2022-05-20 09:50:47 +0000

    途中

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -2,17 +2,33 @@ class Public::OrdersController < ApplicationController
   layout 'public/application'
 
   def new
+    @order = Order.new
+    @deliveries = Delivery.all
+    @delivery = Delivery.new
+  end
+  
+  def create
+    @order = Order.new(order_params)
   end
 
   def confirm
+    @order = Order.find(params[:id])
   end
 
   def complete
   end
 
   def index
+    @orders = Order.all
   end
 
   def show
+    @order = Order.find(params[:id])
+  end
+  
+  private
+  
+  def order_params
+    params.require(:order).permit(:pay_way,:postcode, :address, :adress_name)
   end
 end
