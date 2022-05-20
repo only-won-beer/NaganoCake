eacf5edc02f249d95e87fb7af81024f771075631
EC2 Default User <ec2-user@ip-172-31-24-178.ec2.internal>
2022-05-20 07:46:56 +0000

    無理で～す

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,6 +1,7 @@
 class CartItem < ApplicationRecord
 
   belongs_to:costomer
-  belongs_to:order_detail
+  # belongs_to:order_detail
+  belongs_to:item
 
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -7,6 +7,7 @@ class Customer < ApplicationRecord
   has_many :deliveries
   has_many :cart_items
   has_many :orders
+  has_many :items, through: :cart_items
 
   def full_name
     self.last_name + self.first_name
