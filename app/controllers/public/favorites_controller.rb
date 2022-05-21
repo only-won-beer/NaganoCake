d7b32417a4199ce87c6468da1cc578f275b6bf65
EC2 Default User <ec2-user@ip-172-31-24-178.ec2.internal>
2022-05-21 17:29:16 +0000

    [Add]いいね機能非同期化

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -1,17 +1,15 @@
 class Public::FavoritesController < ApplicationController
   
   def create
-    item = Item.find(params[:item_id])
-    favorite = current_customer.favorites.new(item_id: item.id)
+    @item = Item.find(params[:item_id])
+    favorite = current_customer.favorites.new(item_id: @item.id)
     favorite.save
-    redirect_to request.referer
   end
 
   def destroy
-    item = Item.find(params[:item_id])
-    favorite = current_customer.favorites.find_by(item_id: item.id)
+    @item = Item.find(params[:item_id])
+    favorite = current_customer.favorites.find_by(item_id: @item.id)
     favorite.destroy
-    redirect_to request.referer
   end
   
 end
