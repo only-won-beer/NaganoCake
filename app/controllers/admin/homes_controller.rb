6343e44a4ed8371d35ee662ca64bc7c7ce536a00
EC2 Default User <ec2-user@ip-172-31-35-116.ap-northeast-1.compute.internal>
2022-05-24 08:38:56 +0000

    オーダー一覧見やすく

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,6 +1,16 @@
 class Admin::HomesController < ApplicationController
   layout 'admin/application'
   def top
-    @orders = Order.page(params[:page])
+    if params[:status] == "ichiran"
+      @orders = Order.all.page(params[:page]).per(10)
+    elsif params[:status]
+      @order_statuses = Order.where(status: params[:status])
+      @orders = @order_statuses.page(params[:page]).per(10)
+    else
+      @orders = Order.all.page(params[:page]).per(10)
+    end
+  end
+  def order_status_params
+    params.require(:order).permit(:status)
   end
 end
